3b6c10d9f443fb5c9b1061f4059dd58fa8885817
g-schaffer <guillaume.schaffer@protonmail.com>
2022-12-08 00:17:20 +0100

    day 6

---
+++ day6/part1.py
@@ -0,0 +1,30 @@
+
+#  []
+def main():
+    with open('data.txt') as f:
+        data = f.read()
+    print(data)
+    sub = data[0:4]
+    print(sub)
+    for i in range(3, len(data)):
+        print(sub)
+        sub = data[i-3:i+1]
+        if unique(sub):
+            print(i + 1)
+            break
+    return True
+
+def unique(string):
+    for i in range(len(string)):
+        for j in range(i+1, len(string)):
+            if string[i] == string[j]:
+                return False
+    return True
+
+def modif_stack(stacks,  i, j):
+    # print(stacks, i , j)
+
+    stacks[j].append(stacks[i].pop())
+    return stacks
+
+main()
